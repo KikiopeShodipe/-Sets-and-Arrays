f606c06302d4ee242162e43b650b75992b8b53d2
KikiopeShodipe <gilliansifuma@gmail.com>
2025-05-07 17:09:09 +0000

    Change

--- main2.py
+++ main2.py
@@ -2,6 +2,6 @@
 set1 = {"green", "blue"}
 set2 = {"blue", "yellow"}
 # Find the intersection
-intersection_set = set1.intersection(set2)
+intersection_set = set1.symmetric_difference(set2)
 # Print the result
 print("value found in both set1 and set2:", intersection_set)
